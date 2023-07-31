c5236dc97dfd71e7dccd88ebf76e7d72c229cd93
DimaMen265 <menzak87@gmail.com>
2023-07-31 17:08:13 +0200

    upd

--- swiper.js
+++ swiper.js
@@ -1,10 +1,16 @@
 const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
     spaceBetween: 16,
+    speed: 1000,
     loop: true,
     mousewheel: true,
     keyboard: true,
     
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false, 
+      },
+      
   pagination: {
       el: '.swiper-pagination',
       clickable: true,
